6e926aa51196865278874973f1050a53b79c2a1b
henrycorbell <henrycorbell@users.noreply.github.com>
2017-10-13 11:46:07 +1100

    tweaks

--- sketches/beta_track/beta_track.ino
+++ sketches/beta_track/beta_track.ino
@@ -1,34 +1,39 @@
 #include <Servo.h>
 Servo myservo;
-int pos = 0;
+int pos = 0;  
   
 void setup() {
   Serial.begin(9600);
 }
 
 void select() {
-int rx1 = analogRead(A1);
-int rx2 = analogRead(A2);
-  float u1 = rx1 * (5.0 / 1023.0);
-  float u2 = rx2 * (5.0 / 1023.0);
-  float v1 = u1 * 5.0;
-  float v2 = u2 * 5.0;//convert analog read to voltage 0-5v
-  Serial.print("rx 1: ");
-  Serial.print(v1);
-  Serial.println("");
-  Serial.print("rx 2: ");
-  Serial.print(v2);
-  Serial.println("");
-  delay(3000);
-}
-
-void turn() {
   int rx1 = analogRead(A1);
   int rx2 = analogRead(A2);
   float u1 = rx1 * (5.0 / 1023.0);
   float u2 = rx2 * (5.0 / 1023.0);
   float v1 = u1 * 5.0;
-  float v2 = u2 * 5.0;//convert analog read to voltage 0-5v
+  float v2 = u2 * 5.0;
+//////////////////////////////////////
+
+  Serial.print("rx 1: ");     ////
+  Serial.print(v1);           //
+  Serial.println("");         //
+  Serial.print("rx 2: ");     //  print rx values
+  Serial.print(v2);           //  
+  Serial.println("");         ////
+  delay(3000);
+  
+}
+
+void choose() {
+  int rx1 = analogRead(A1);
+  int rx2 = analogRead(A2);
+  float u1 = rx1 * (5.0 / 1023.0);
+  float u2 = rx2 * (5.0 / 1023.0);
+  float v1 = u1 * 5.0;
+  float v2 = u2 * 5.0;
+//////////////////////////////////////  
+
   if(v1 >= 1.05) {
     Serial.println("RX1 HIGH");
   }
@@ -41,38 +46,40 @@ void turn() {
   else if(v2 <= 0.5) {
     Serial.println("RX2 LOW");
   }
+  
 }
 
-void choose() {
+void turn() {
   int rx1 = analogRead(A1);
   int rx2 = analogRead(A2);
   float u1 = rx1 * (5.0 / 1023.0);
   float u2 = rx2 * (5.0 / 1023.0);
   float v1 = u1 * 5.0;
-  float v2 = u2 * 5.0;//convert analog read to voltage 0-5v
-  int option = 0;
+  float v2 = u2 * 5.0;
+//////////////////////////////////////
 
+  int option = 0;
   if(v1>v2) {
     int option = 1;
     Serial.println("RX1");
     myservo.attach(9);
-    pos = 180;
+    pos = 175;
     myservo.write(pos);
+    delay(500);
   }
   else if(v1<v2) {
     int option = 2;
     Serial.println("RX2");
     myservo.attach(9);
-    pos = 1;
+    pos = 5;
     myservo.write(pos);
+    delay(500);
   }
-}
-
-void servo(){
-    
+  
 }
 
 void loop() {
   select();
   choose();
+  turn();
 }
